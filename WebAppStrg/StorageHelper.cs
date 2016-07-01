7cb3a3cb510cb813a9e37311f07e62e4cad2fde7
catherine wang <cawa@microsoft.com>
2016-07-01 00:33:14 +0000

    Add project files.

---
+++ WebAppStrg/StorageHelper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.WindowsAzure;
+
+using Microsoft.Azure;
+
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace WebAppStrg
+{
+    public class StorageHelper
+    {
+        string connectionString = CloudConfigurationManager.GetSettingFromSecret("ImageStore");
+
+        CloudStorageAccount sa = CloudStorageAccount.Parse(connectionString);
+
+        public void addBlob(string filePath)
+        {
+
+        }
+
+        public void downloadBlob(string fileName, string outputPath)
+        {
+
+        }
+    }
+}
